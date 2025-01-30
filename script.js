197a6c15a5c81af38ecd29f14b723cf8e2bba132
Grace Buttler <grace.buttler@gmail.com>
2025-01-30 15:39:41 +0000

    begin creating game in js

---
+++ script.js
@@ -0,0 +1,19 @@
+//gameboard factory funtion stored in an IIFE
+
+(function gameboard() {
+  console.log("gameboard has been run");
+  [(" ", " ", " ", " ", " ", " ", " ", " ", " ")];
+})();
+
+//player object
+const player = {};
+
+//playGame function
+const playGame = function () {};
+
+//I NEED
+//create and store gameboard
+//factory to make players
+//function to keep track of player turn
+//function that lets a player place a marker, and checks if space is free
+//function that checks for wins after each turn
