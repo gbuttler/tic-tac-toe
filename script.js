5a5c83c264684fcd6cc17defb6aa05a15e00816a
Grace Buttler <grace.buttler@gmail.com>
2025-01-30 19:29:45 +0000

    begging of js logic

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //gameboard factory funtion stored in an IIFE
 
-(function gameboard() {
-  console.log("gameboard has been run");
-  [(" ", " ", " ", " ", " ", " ", " ", " ", " ")];
+const gameboard = (function createGameboard() {
+  console.log("created gamedboard");
+  let displayBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  const showBoard = () => {
+    const topLine = displayBoard.slice(0, 3);
+    const midLine = displayBoard.slice(3, 6);
+    const botLine = displayBoard.slice(6, 9);
+    console.log(topLine);
+    console.log(midLine);
+    console.log(botLine);
+  };
+  const playTurn = (a, b) => {
+    displayBoard[a] = b;
+    showBoard();
+  };
+  return { showBoard, playTurn };
 })();
 
 //player object
@@ -11,9 +24,8 @@ const player = {};
 //playGame function
 const playGame = function () {};
 
-//I NEED
-//create and store gameboard
-//factory to make players
-//function to keep track of player turn
-//function that lets a player place a marker, and checks if space is free
-//function that checks for wins after each turn
+//NEXT STEPS
+//write function to keep track of player turn
+//write function to allow player to enter o or x and nothing else
+//write function to check that space is empty
+//write function to check if someone has won or drawn
