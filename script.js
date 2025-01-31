db6972879977df09b18b6ff217f03acdbd0d3c29
Grace Buttler <grace.buttler@gmail.com>
2025-01-31 14:47:45 +0000

    working console game completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //gameboard factory funtion stored in an IIFE
 
-const gameboard = (function createGameboard() {
-  console.log("created gamedboard");
+const gamePlay = (function createGameboard() {
+  console.log("created gameboard");
   let displayBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   const showBoard = () => {
     const topLine = displayBoard.slice(0, 3);
@@ -11,21 +11,117 @@ const gameboard = (function createGameboard() {
     console.log(midLine);
     console.log(botLine);
   };
-  const playTurn = (a, b) => {
-    displayBoard[a] = b;
-    showBoard();
+
+  //boolean to decide whose turn it is
+  let xTurn = true;
+
+  //track turn number
+  let turnNumber = 1;
+
+  //check if there is a winner
+  let foundWinner = false;
+
+  //alternates whose turn it is
+  const whoTurn = () => {
+    if (xTurn === true) {
+      console.log("it is now the turn of player o");
+      xTurn = false;
+    } else {
+      console.log("it is now the turn of player x");
+      xTurn = true;
+    }
   };
-  return { showBoard, playTurn };
+
+  //lets you play a move by entering a grid number and then an x or o
+  const playTurn = (a, b) => {
+    if (xTurn === true) {
+      b = "x";
+    } else {
+      b = "o";
+    }
+    //if statement to stop cells overlapping
+    if (displayBoard[a] !== " ") {
+      console.log("Please choose another square");
+    } else {
+      displayBoard[a] = b;
+      showBoard();
+      checkWin();
+
+      //stops the game if someone has won
+      if (foundWinner === true) {
+        console.log("Game Over");
+      } else {
+        turnNumber++;
+        whoTurn();
+        console.log(`this is now turn ${turnNumber}`);
+      }
+    }
+  };
+
+  //console statement for who has won
+  const winnerIs = () => {
+    if (xTurn === true) {
+      console.log("Player x has won");
+      foundWinner = true;
+    } else {
+      console.log("Player o has won");
+      foundWinner = true;
+    }
+  };
+
+  //check for a winner
+  const checkWin = () => {
+    if (
+      //check each row for a winner
+      (displayBoard[0] === displayBoard[1] &&
+        displayBoard[1] === displayBoard[2] &&
+        displayBoard[0] !== " ") ||
+      (displayBoard[3] === displayBoard[4] &&
+        displayBoard[4] === displayBoard[5] &&
+        displayBoard[3] !== " ") ||
+      (displayBoard[6] === displayBoard[7] &&
+        displayBoard[7] === displayBoard[8] &&
+        displayBoard[6] !== " ")
+    ) {
+      console.log("row win");
+      foundWinner = true;
+      winnerIs();
+    } else if (
+      //check each column for a winner
+      (displayBoard[0] === displayBoard[3] &&
+        displayBoard[3] === displayBoard[6] &&
+        displayBoard[0] !== " ") ||
+      (displayBoard[1] === displayBoard[4] &&
+        displayBoard[4] === displayBoard[7] &&
+        displayBoard[1] !== " ") ||
+      (displayBoard[2] === displayBoard[5] &&
+        displayBoard[5] === displayBoard[8] &&
+        displayBoard[2] !== " ")
+    ) {
+      console.log("column win");
+      winnerIs();
+      foundWinner = true;
+    } else if (
+      //check each diagonal for a winner
+      (displayBoard[0] === displayBoard[4] &&
+        displayBoard[4] === displayBoard[8] &&
+        displayBoard[0] !== " ") ||
+      (displayBoard[2] === displayBoard[4] &&
+        displayBoard[4] === displayBoard[6] &&
+        displayBoard[2] !== " ")
+    ) {
+      console.log("diagonal win");
+      winnerIs();
+      foundWinner = true;
+    } else if (turnNumber === 8) {
+      //check for tie
+      console.log("it is a tie");
+      foundWinner = true;
+    }
+  };
+
+  return { showBoard, playTurn, whoTurn, checkWin };
 })();
 
-//player object
-const player = {};
-
-//playGame function
-const playGame = function () {};
-
 //NEXT STEPS
-//write function to keep track of player turn
-//write function to allow player to enter o or x and nothing else
-//write function to check that space is empty
-//write function to check if someone has won or drawn
+//check to see if square is empty - no overwriting allowed
